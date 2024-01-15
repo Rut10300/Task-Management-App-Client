f1f28c0ef3a77d172fa06cce1a9504af1cd8591d
Rut10300 <Rut10300@gmail.com>
2024-01-15 03:53:41 +0200

    HI

--- src/components/Search/TodoSearch.jsx
+++ src/components/Search/TodoSearch.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
-export default function TodoSearch({setsearchTodoFlag,searchTodos,handleSearchChange,searchParams}) {
+export default function TodoSearch({ setsearchTodoFlag, searchTodos, handleSearchChange, searchParams }) {
   return (
-    <div style={{ zIndex: "1", backgroundColor: "lightskyblue", position: "absolute", border: "2px solid black", top: "30vh", right: "40vw", padding: "2vh" }}>
-    <button onClick={() => {
-      setsearchTodoFlag(false);
-    }} style={{ backgroundColor: "rgba(255, 255, 255, 0)" }}>✖️</button>
-    <h3>search Task</h3>
-    <label htmlFor="taskId">Task Id</label><br />
-    <input id="taskId" className='notTouch' name="taskId" value={searchParams.taskId} onChange={(e) => {handleSearchChange(e.target.value,e.target.name);e.preventDefault()}} type="number" placeholder="12" min="1" /><br />
-    <label htmlFor="title">Title</label><br />
-    <input id="title" className='notTouch' name="title" value={searchParams.title} type="text" required onChange={(e) => {handleSearchChange(e.target.value,e.target.name);e.preventDefault()}} /><br />
-    <label htmlFor="completed">Completed</label><br />
-    <input type="text" name="completed" value={searchParams.completed} placeholder='yes / no' onChange={(e) => {handleSearchChange(e.target.value,e.target.name);e.preventDefault()}} /><br />
-    <button type="sumbit" id='submitButton' className='submit'
-      onClick={(e) => { e.preventDefault(); searchTodos(); setsearchTodoFlag(false);  }}>search</button>
-  </div>
+    <div className="back">
+      <div className='searchCompo'>
+        <button onClick={() => {
+          setsearchTodoFlag(false);
+        }} style={{ backgroundColor: "rgba(255, 255, 255, 0)" }}>✖️</button>
+        <h3>search Task</h3>
+        <label htmlFor="taskId">Task Id</label><br />
+        <input id="taskId" className='notTouch' name="taskId" value={searchParams.taskId} onChange={(e) => { handleSearchChange(e.target.value, e.target.name); e.preventDefault() }} type="number" placeholder="12" min="1" /><br />
+        <label htmlFor="title">Title</label><br />
+        <input id="title" className='notTouch' name="title" value={searchParams.title} type="text" required onChange={(e) => { handleSearchChange(e.target.value, e.target.name); e.preventDefault() }} /><br />
+        <label htmlFor="completed">Completed</label><br />
+        <input type="text" name="completed" value={searchParams.completed} placeholder='yes / no' onChange={(e) => { handleSearchChange(e.target.value, e.target.name); e.preventDefault() }} /><br />
+        <button type="sumbit" id='submitButton' className='submit'
+          onClick={(e) => { e.preventDefault(); searchTodos(); setsearchTodoFlag(false); }}>search</button>
+      </div>
+    </div>
   );
 }
